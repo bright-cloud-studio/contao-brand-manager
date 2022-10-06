9dded81dbe86d12b2293c83d4168424a2d52fdf5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:50:17 -0400

    Create modules.php

---
+++ system/modules/contao_brand_manager/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['brands'] = array('Brands', 'Configure Brands');
