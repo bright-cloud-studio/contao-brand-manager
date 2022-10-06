7d53eccf3579f8730d6617ec602ae1d009d7d969
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:51:22 -0400

    Create tl_brand.php

---
+++ system/modules/contao_brand_manager/languages/en/tl_brand.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_brand']['location_legend'] 	= 'Rep Name Fields';
+$GLOBALS['TL_LANG']['tl_brand']['address_legend'] 	= 'Red Address Fields';
+$GLOBALS['TL_LANG']['tl_brand']['website_legend'] 	= 'Rep Website Details';
+$GLOBALS['TL_LANG']['tl_brand']['publish_legend'] 	= 'Publish';
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_brand']['alias'] 			          = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_brand']['published'] 		        = array('Published', 'Show this record on the front end.');
+
+/* Lamvin Unique Fields */
+$GLOBALS['TL_LANG']['tl_brand']['name'] 			          = array('Brand Name', 'Enter the brand\'s name');
+
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_brand']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_brand']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_brand']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_brand']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_brand']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_brand']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
