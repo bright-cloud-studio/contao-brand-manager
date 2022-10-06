a0f413a820052c0561a45e6754358ece63129e7a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:24:25 -0400

    Create GetBrandContent.php

---
+++ system/modules/contao_brand_manager/library/bcs/GetBrandContent.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+namespace bcs;
+use Contao\DataContainer;
+use Contao\ContentElement;
+
+class GetBrandContent extends \System
+{
+	public function onReplaceTag (string $insertTag)
+	{
+		// if this tag doesnt contain :: it doesn't have an id, so we can stop right here
+		if (stristr($insertTag, "::") === FALSE) {
+			return 'Your tag has no ID. Please add a User ID or remove this tag from the page.';
+		}
+
+		// break our tag into an array
+		$arrTag = explode("::", $insertTag);
+
+		// lets make decisions based on the beginning of the tag
+		switch($arrTag[0]) {
+			// if the tag is what we want, {{simple_inventory::id}}, then lets go
+			case 'brand':
+				return "Serving Brand content";
+			break;
+		}
+
+		// something has gone horribly wrong, let the user know and hope for brighter lights ahead
+		return 'Your tag is improperly formatted. Please try again.';
+	}
+}
