542c7d0b2d6d253f3c68201c25b1deb79ad14e57
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:19:43 -0400

    Create autoload.php

---
+++ system/modules/contao_brand_manager/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+  'bcs\GetBrandContent' 		=> 'system/modules/contao_brand_manager/library/bcs/GetBrandContent.php'
+));
