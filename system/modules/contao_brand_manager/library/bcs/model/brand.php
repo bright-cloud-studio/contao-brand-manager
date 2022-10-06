c5780317fc2876cb3c50d2633f8e61719297be80
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:37:09 -0400

    Create brand.php

---
+++ system/modules/contao_brand_manager/library/bcs/model/brand.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+namespace bcs\model;
+
+class Brand extends \Model
+{
+	/* Table name */
+	protected static $strTable = 'tl_brand';	
+}
