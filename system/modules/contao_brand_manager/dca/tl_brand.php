15d82f0829b7a6651348df3ac7df1e5fe183541a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-06 15:34:03 -0400

    Create tl_brand.php

---
+++ system/modules/contao_brand_manager/dca/tl_brand.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+/* Table tl_location */
+$GLOBALS['TL_DCA']['tl_brand'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'alias' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('name'),
+            'flag'                    => 1,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('name'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'export' => array
+            (
+                'label'               => 'Export Brands CSV',
+                'href'                => 'key=exportBrands',
+                'icon'                => 'system/modules/contao_brand_manager/assets/icons/file-export-icon-16.png'
+            ),
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_brand']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_brand']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_brand']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+		(
+			'label'               => &$GLOBALS['TL_LANG']['tl_brand']['toggle'],
+			'icon'                => 'visible.gif',
+			'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+			'button_callback'     => array('bcs\backend\brands', 'toggleIcon')
+		),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_brand']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{brand_legend},name,alias;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                     		=> "int(10) unsigned NOT NULL default '0'"
+        ),
+		'sorting' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'alias' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_brand']['alias'],
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'search'                  => true,
+			'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+			'save_callback' => array
+			(
+				array('bcs\backend\brands', 'generateAlias')
+			),
+			'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+
+		),
+		'name' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_brand']['name'],
+			'inputType'               => 'text',
+			'default'		  => '',
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'published' => array
+		(
+			'exclude'                 => true,
+			'label'                   => &$GLOBALS['TL_LANG']['tl_brand']['published'],
+			'inputType'               => 'checkbox',
+			'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		)		
+    )
+);
