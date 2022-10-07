6615bf5f71f0fe5f67dfec1d217a4d448a96f8ff
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-07 14:05:45 -0400

    Update tl_page.php

--- system/modules/contao_brand_manager/dca/tl_page.php
+++ system/modules/contao_brand_manager/dca/tl_page.php
@@ -12,17 +12,15 @@
 
  /* Extend the tl_page palette */
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{publish_legend}', ';{page_tag_navigation_legend},page_tag_navigation_target,page_tag_navigation_anchor_target;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{publish_legend}', ';{brand_select_legend},brand_select;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback'] = str_replace(';{publish_legend}', ';{brand_select_legend},brand_select;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback']);
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback'] = str_replace(';{publish_legend}', ';{page_tag_navigation_legend},page_tag_navigation_target,page_tag_navigation_anchor_target;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback']);
-
-$GLOBALS['TL_DCA']['tl_page']['fields']['page_tag_navigation_anchor_target'] = array
+$GLOBALS['TL_DCA']['tl_page']['fields']['brand_select'] = array
 (
-	'label'			        => &$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_anchor_target'],
-	'inputType'             => 'text',
-    'default'               => '',
-    'search'                => true,
-    'eval'                  => array('mandatory'=>false, 'tl_class'=>'w50'),
-    'sql'                   => "varchar(255) NOT NULL default ''"
+	'sql'                   => "varchar(255) NOT NULL default ''",
+	'label'			        => &$GLOBALS['TL_LANG']['tl_page']['brand_select'],
+	'inputType'             => 'radio',
+	'options_callback'	    => array('Bcs\Backend\Brands', 'optionBrands'),										
+	'eval'                  => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'w50') 
 );
 
