cebb4e114a605e7f5b983073adb339bce68a14f4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-07 13:55:40 -0400

    Update tl_page.php

--- system/modules/contao_brand_manager/dca/tl_page.php
+++ system/modules/contao_brand_manager/dca/tl_page.php
@@ -12,15 +12,24 @@
 
  /* Extend the tl_page palette */
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{publish_legend}', ';{brand_select_legend},brand_select;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace(';{publish_legend}', ';{page_tag_navigation_legend},page_tag_navigation_target,page_tag_navigation_anchor_target;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
 
-$GLOBALS['TL_DCA']['tl_page']['fields']['brand'] = array
+$GLOBALS['TL_DCA']['tl_page']['fields']['page_tag_navigation_target'] = array
 (
-    'label'                     => &$GLOBALS['TL_LANG']['tl_page']['brand_select'],
-    'inputType'                 => 'select',
-    'default'                   => '',
-    'options_callback'          => array('Bcs\Backend\Brands', 'optionsBrands'),
-    'eval'                      => array('includeBlankOption'=>false, 'chosen'=>true, 'tl_class'=>'w50'),
-    'sql'                       => "varchar(255) NOT NULL default ''"
+	'sql'                   => "varchar(255) NOT NULL default ''",
+	'label'			        => &$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_target'],
+	'inputType'             => 'radio',
+	'options_callback'	    => array('Bcs\Backend\ChildCategoryBackend', 'getChildCategories'),										
+	'eval'                  => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'w50') 
+);
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['page_tag_navigation_anchor_target'] = array
+(
+	'label'			        => &$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_anchor_target'],
+	'inputType'             => 'text',
+    'default'               => '',
+    'search'                => true,
+    'eval'                  => array('mandatory'=>false, 'tl_class'=>'w50'),
+    'sql'                   => "varchar(255) NOT NULL default ''"
 );
 
