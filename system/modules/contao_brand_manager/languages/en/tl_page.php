c5979f36274e9e621207f93f6a3525e1354646a9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-07 13:49:56 -0400

    Create tl_page.php

---
+++ system/modules/contao_brand_manager/languages/en/tl_page.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Brand Manager
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-brand-manager
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_page']['brand_select_legend'] = 'Brand Manager Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_page']['brand_select']          = array('Select Brand', 'Assign a Brand to this Site Root');
