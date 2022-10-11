8bd44899f37e4552a302b071c7b8fbf7788a8052
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-11 13:16:17 -0400

    Update config.php

--- system/modules/contao_brand_manager/config/config.php
+++ system/modules/contao_brand_manager/config/config.php
@@ -28,5 +28,5 @@ $GLOBALS['TL_MODELS']['tl_brand'] = 'Bcs\Model\Brand';
 /* Hooks */
 if (\Config::getInstance()->isComplete()) {
   $GLOBALS['TL_HOOKS']['replaceInsertTags'][]   = array('Bcs\GetBrandContent', 'onReplaceTag');
-  $GLOBALS['TL_HOOKS']['generatePage'][]        = array('Bcs\GetBrandContent', 'generateBrandSCSS');
+  $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][]        = array('Bcs\GetBrandContent', 'generateBrandSCSS');
 }
